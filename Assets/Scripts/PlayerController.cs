40db93ae0bcec1516d117fac9ceeab6fd77bf914
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-04-20 12:42:04 -0700

    Update PlayerController.cs
    made temp. turning code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     float horizontalInput;
     float verticalInput;
+    private float speed = 5;
+    private float lookSpeed = 100;
     private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
@@ -21,9 +23,23 @@ public class PlayerController : MonoBehaviour
         //horizontal movement
         //playerRb.AddForce(Vector3.right * Time.deltaTime * horizontalInput * 500,ForceMode.Acceleration);
         //playerRb.AddForce(Vector3.forward * Time.deltaTime * verticalInput * 500, ForceMode.Acceleration);
-        transform.Translate(Vector3.forward * verticalInput);
-/*
-        //look at mouse
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //temporary turning code to make sure that the forces work
+        if (Input.GetKey(KeyCode.U))
+        {
+            transform.Rotate(-Vector3.up * Time.deltaTime * lookSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * lookSpeed);
+        }
+
+
+        /*
+         * david's spaghetti code
+         * //look at mouse
         Ray ray;
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
