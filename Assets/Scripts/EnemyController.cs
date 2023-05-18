693039cddda233bd90842ca07ed1d3ef855c1592
dav <usr21p1@gmail.com>
2023-05-18 12:04:26 -0700

    Add slightly smarter enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,12 +12,15 @@ public class EnemyController : MonoBehaviour
     private Taggable taggable;
     private TagManager tagManager;
 
+    private Vector3 lastGotoPoint;
+
     // Start is called before the first frame update
     void Start()
     {
         tagManager = TagManager.GetTagManager();
         navAgent = GetComponent<NavMeshAgent>();
         taggable = GetComponent<Taggable>();
+        lastGotoPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -27,11 +31,17 @@ public class EnemyController : MonoBehaviour
             navAgent.speed = speed;
             var closest = tagManager.FindNearestPlayer(transform);
             navAgent.SetDestination(closest.position + Vector3.up);
+            lastGotoPoint = transform.position;
         }
         else
         {
-            //  TODO: Very naive implementation.
-            navAgent.SetDestination(transform.position - (tagManager.GetWhoIsIt().transform.position - transform.position).normalized);
+            if (Vector3.Distance(lastGotoPoint, transform.position) < 5.0f)
+            {
+                var taggerPosition = tagManager.GetWhoIsIt().transform.position;
+                var dst = EnemyController.FindBestDestination(taggerPosition);
+                navAgent.SetDestination(dst);
+                lastGotoPoint = dst;
+            }
         }
     }
 
@@ -42,5 +52,62 @@ public class EnemyController : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
             tagManager.FindNearestPlayer(transform);
         }
+    }
+
+    static List<(float, Vector3)> SortArray(ref List<(float, Vector3)> zipped)
+    {
+        var n = zipped.Count;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (zipped[j].Item1 > zipped[j + 1].Item1)
+                {
+                    var tempVar = zipped[j];
+                    zipped[j] = zipped[j + 1];
+                    zipped[j + 1] = tempVar;
+                }
+        return zipped;
+    }
+
+    static Vector3 FindBestDestination(Vector3 taggerPosition)
+    {
+        var pepperonis = SubdivideGround.GetAllPoints();
+        var pepperonisWeights = new List<float>();
+
+        foreach (var pepperoni in pepperonis)
+        {
+            var val = (Vector3.Dot(taggerPosition.normalized, pepperoni.normalized) * -1 + 1) / 2;
+            pepperonisWeights.Add(val);
+        }
+
+        float totalWeight = 0.0f;
+
+        foreach (var weight in pepperonisWeights)
+        {
+            totalWeight += weight;
+        }
+
+        var zippedPepperonis = new List<(float, Vector3)>();
+
+        for (int i = 0; i < pepperonis.Count; i++)
+        {
+            zippedPepperonis.Add((pepperonisWeights[i], pepperonis[i]));
+        }
+
+        var sortedZippedPepperonis = SortArray(ref zippedPepperonis);
+
+        float cumulativeWeight = 0.0f;
+        float randomNumber = Random.Range(0.0f, totalWeight);
+
+        foreach (var (weight, pepperoni) in sortedZippedPepperonis)
+        {
+            cumulativeWeight += weight;
+            if (randomNumber < cumulativeWeight)
+            {
+                return pepperoni;
+            }
+        }
+
+        int randomIndex = Random.Range(0, pepperonis.Count);
+        return pepperonis[randomIndex];
     }
 }
---
+++ Assets/Scripts/SubdivideGround.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubdivideGround : MonoBehaviour
+{
+    BoxCollider ground;
+    List<Vector3> points;
+
+    const int N = 10;
+
+    void Start()
+    {
+        points = new List<Vector3>();
+        ground = GetComponent<BoxCollider>();
+
+        var origin = ground.center - new Vector3(ground.bounds.size.x / 2, 0.0f, ground.bounds.size.z / 2);
+        var xDiv = ground.bounds.size.x / N;
+        var yDiv = ground.bounds.size.z / N;
+        for (int xI = 0; xI < N + 1; xI++)
+        {
+            for (int yI = 0; yI < N + 1; yI++)
+            {
+                var p = origin + new Vector3(xI * xDiv, 0.0f, yI * yDiv);
+                points.Add(p);
+            }
+        }
+    }
+
+    List<Vector3> GetPoints()
+    {
+        return points;
+    }
+
+    public static List<Vector3> GetAllPoints()
+    {
+        var total = new List<Vector3>();
+        var objects = GameObject.FindObjectsOfType<SubdivideGround>();
+        foreach (var sub in objects)
+        {
+            var points = sub.GetPoints();
+            foreach (var point in points)
+            {
+                total.Add(point);
+            }
+        }
+        return total;
+    }
+}
